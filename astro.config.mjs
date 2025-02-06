b23dc0a4e8f71811cf792d8a9a07a82760a0c35f
hechtron <jon@smartassdesign.com>
2025-02-06 17:05:42 -0500

    finished tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-tutorialify.netlify.app"
-  });
-  
+  site: "https://astro-tutorialify.netlify.app",
+  integrations: [preact()]
+});
